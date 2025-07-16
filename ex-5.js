1ee4b33bd14e66248b88df771b593b379071bb15
Ponlawat <p.sangtub@gmail.com>
2025-07-16 14:33:28 +0700

    feat: add variable initializations and console logs for exercises 1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000
+let discountValue = totalPrice * 0.25
+let totalPriceAfterDiscount = totalPrice - discountValue
+
+console.log(totalPriceAfterDiscount);
